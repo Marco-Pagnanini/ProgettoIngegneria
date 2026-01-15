1292f9f33ca510b9ff4c223ff1e0612aa2abd335
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2026-01-15 17:53:04 +0100

    Add User and hackathon

---
+++ Progetto/app/src/main/java/org/example/models/User.java
@@ -0,0 +1,17 @@
+package org.example.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String cellulare;
+    private LocalDate dataNascita;
+    private LocalDateTime dataCreazione;
+
+
+}
