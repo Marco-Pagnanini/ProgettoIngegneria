3b6668ed3da0c11a5516f25967ee2fd6685022fc
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2026-01-17 12:35:13 +0100

    Add Validator and HackathonValidator

--- Progetto/app/src/main/java/org/example/Application/Service/HackathonService.java
+++ Progetto/app/src/main/java/org/example/Application/Service/HackathonService.java
@@ -1,6 +1,7 @@
 package org.example.Application.Service;
 
 import org.example.Application.Abstraction.Service.IHackathonService;
+import org.example.Application.Abstraction.Validator.Validator;
 import org.example.Core.models.Hackathon;
 import org.example.utils.UnitOfWork.UnitOfWork;
 
@@ -9,20 +10,27 @@ import java.util.List;
 public class HackathonService implements IHackathonService {
 
     private final UnitOfWork unitOfWork;
+    private final Validator<Hackathon> hackathonValidator;
 
-    public HackathonService(UnitOfWork unitOfWork) {
+
+    public HackathonService(UnitOfWork unitOfWork, Validator<Hackathon> hackathonValidator) {
         this.unitOfWork = unitOfWork;
+        this.hackathonValidator = hackathonValidator;
     }
 
     @Override
     public Hackathon addHackathon(Hackathon hackathon) {
+        if(!hackathonValidator.validate(hackathon)) return null;
+
         unitOfWork.hackathonRepository().create(hackathon);
         unitOfWork.saveChanges();
+
         return hackathon;
     }
 
     @Override
     public Hackathon updateHackathon(Hackathon hackathon) {
+        if(!hackathonValidator.validate(hackathon)) return null;
         Hackathon response = unitOfWork.hackathonRepository().update(hackathon);
         unitOfWork.saveChanges();
         return response;
