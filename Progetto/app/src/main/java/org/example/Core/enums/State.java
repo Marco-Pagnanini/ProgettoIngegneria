83c1fda2b551ff385b7f3baeaac72757774591f4
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2026-01-28 12:07:42 +0100

    Add IN_VALUTAZIONE state

--- Progetto/app/src/main/java/org/example/Core/enums/State.java
+++ Progetto/app/src/main/java/org/example/Core/enums/State.java
@@ -4,13 +4,15 @@ public enum State {
     IN_ISCRIZIONE,
     IN_PREPARAZIONE,
     IN_CORSO,
+    IN_VALUTAZIONE,
     CONCLUSO;
 
     public State next(){
         return switch (this){
             case IN_ISCRIZIONE -> IN_PREPARAZIONE;
             case IN_PREPARAZIONE -> IN_CORSO;
-            case IN_CORSO, CONCLUSO -> CONCLUSO;
+            case IN_CORSO -> IN_VALUTAZIONE;
+            case IN_VALUTAZIONE,CONCLUSO -> CONCLUSO;
         };
     }
 }
